1ae6af5d60e28c378608ddc3ef7b87b56c6f9b79
André Salazar <Zombiewafle>
2019-01-13 00:47:55 -0600

    First and Problaly de last commit

---
+++ Assets/EasterEgg.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class EasterEgg : MonoBehaviour
+{
+
+    private Texture2D Icono;
+    private int CoordenadaX = 0;
+    private int CoordenadaY = 0;
+    private int ScaleX = 0;
+    private int ScaleY = 0;
+
+
+        // Start is called before the first frame update
+        void Start(){
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
